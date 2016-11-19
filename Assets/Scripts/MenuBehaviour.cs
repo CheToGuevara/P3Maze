19b77e2d6f8a4d6fc09a224a70cbc779951dbe33
CheToGuevara <aaronsujar@gmail.com>
2016-11-19 19:49:12 +0100

    feat: Finish the first version of the menu
    I create a Menu as the course, with a box and using the gvrreticle and
    the Unity events to create a interactive menu.

---
+++ Assets/Scripts/MenuBehaviour.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MenuBehaviour : MonoBehaviour {
+
+	bool looking= false;
+	public int m_scene;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (looking)
+		{
+			
+			this.transform.Rotate(0,1,0);
+		}
+	}
+
+
+
+	public void onPointEnter()
+	{
+		
+		looking = true;
+	}
+
+	public void onPointExit()
+	{
+		
+		looking = false;
+	}
+
+	public void onPointClicked()
+	{
+		SceneManager.LoadScene(m_scene);
+	}
+}
