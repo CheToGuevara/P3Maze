5a36aac2c5eaca70f383b201ff0c7a6f5f720842
CheToGuevara <aaronsujar@gmail.com>
2016-11-22 22:40:35 +0100

    feat: Player script and Finish script
    Player script is for save the coin and key information.
    Finish script is play the particle system

---
+++ Assets/Scripts/FinishLevel.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class FinishLevel : MonoBehaviour {
+
+
+	bool _ending = false;
+	float _deltaTime = 0;
+	GameObject m_camera;
+	Vector3 _origin,_destiny;
+
+	void OnTriggerEnter(Collider other) 
+	{
+		ParticleSystem m_partSys = this.GetComponent<ParticleSystem> ();
+		if (m_partSys) 
+		{
+			m_partSys.Play ();
+		}
+
+		m_camera = GameObject.Find ("Main Camera");
+		_origin = m_camera.transform.position;
+		_destiny = m_camera.transform.position + new Vector3 (0, 10, 0);
+		_ending = true;
+	}
+
+
+	void update()
+	{
+		if (_ending) {
+			_deltaTime += 0.01f;
+			Camera.main.transform.position = Vector3.Lerp (_origin, _destiny, _deltaTime);
+			if (_deltaTime == 1)
+			{
+				_ending = false;
+				SceneManager.LoadScene(0);
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	bool key_found = false;
+	int coins = 0;
+
+
+
+
+	public void set_key(bool found)
+	{
+		key_found = found;
+	}
+
+	public bool get_key()
+	{
+		return key_found;
+	}
+
+	public void add_coin()
+	{
+		coins++;
+	}
+
+	public int get_coins()
+	{
+		return coins;
+	}
+
+}
